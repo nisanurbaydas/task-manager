8ddd1bb585b249eda37357b2370b0c19ebd80ed7
Nisa <n.nurb@hotmail.com>
2022-06-27 16:03:11 +0300

    install required dependencies, create folder structure, server config and up the server

---
+++ V1/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const config = require('./config');
+
+config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+
+const PORT = process.env.APP_PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
