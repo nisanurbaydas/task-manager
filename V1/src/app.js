ae163f89031e3b35945d9b529e9bb6cd4a35408d
Nisa <n.nurb@hotmail.com>
2022-07-02 19:46:45 +0300

    create project routes

--- V1/src/app.js
+++ V1/src/app.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const config = require('./config');
 const loaders = require('./loaders');
 
+const ProjectRoutes = require('./routes/Project');
+
 config();
 loaders();
 
@@ -13,4 +15,5 @@ app.use(helmet());
 const PORT = process.env.APP_PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
+  app.use('/projects', ProjectRoutes.router);
 });
