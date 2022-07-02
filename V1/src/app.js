544eeff61047e9169cc499dd124797f60cc9653d
Nisa <n.nurb@hotmail.com>
2022-07-02 19:16:14 +0300

    connect to the db

--- V1/src/app.js
+++ V1/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const config = require('./config');
+const loaders = require('./loaders');
 
 config();
+loaders();
 
 const app = express();
 app.use(express.json());
