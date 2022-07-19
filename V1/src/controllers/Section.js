1d26390801fa077c20ef5781108f78a8ef0127b6
Nisa <n.nurb@hotmail.com>
2022-07-19 18:12:44 +0300

    OOP implementation for section service

--- V1/src/controllers/Section.js
+++ V1/src/controllers/Section.js
@@ -1,13 +1,12 @@
 const httpStatus = require('http-status');
 
-const { insert, list, modify, remove } = require('../services/Section');
+//const { insert, list, modify, remove } = require('../services/Section');
+const Service = require('../services/Section');
+const SectionService = new Service();
 
 const index = (req, res) => {
-  if (!req?.params?.projectId)
-    return res
-      .status(httpStatus.BAD_REQUEST)
-      .send({ error: 'Missing information' });
-  list({ project_id: req.params.projectId })
+  if (!req?.params?.projectId) return res.status(httpStatus.BAD_REQUEST).send({ error: 'Missing information' });
+  SectionService.list({ project_id: req.params.projectId })
     .then((response) => {
       res.status(httpStatus.OK).send(response);
     })
@@ -18,7 +17,7 @@ const index = (req, res) => {
 
 const create = (req, res) => {
   req.body.user_id = req.user;
-  insert(req.body)
+  SectionService.create(req.body)
     // res.status(httpStatus.CREATED).send("Section Create");
     .then((response) => {
       res.status(httpStatus.CREATED).send(response);
@@ -30,33 +29,25 @@ const create = (req, res) => {
 
 const update = (req, res) => {
   if (!req.params.id) {
-    return res
-      .status(httpStatus.BAD_REQUEST)
-      .send({ message: 'Missing information' });
+    return res.status(httpStatus.BAD_REQUEST).send({ message: 'Missing information' });
   }
-  modify(req.body, req.params?.id)
+  SectionService.update(req.body, req.params?.id)
     .then((updatedSection) => {
       res.status(httpStatus.OK).send(updatedSection);
     })
-    .catch(() =>
-      res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .send({ error: 'Something went wrong' })
-    );
+    .catch(() => res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: 'Something went wrong' }));
 };
 
 const deleteSection = (req, res) => {
   if (!req.params.id) {
     return res.status(httpStatus.BAD_REQUEST).send('Missing information');
   }
-  remove(req.params?.id)
+  SectionService.delete(req.params?.id)
     .then((deletedItem) => {
       res.status(httpStatus.OK).send(deletedItem);
     })
     .catch(() => {
-      res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .send({ error: 'Something went wrong' });
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: 'Something went wrong' });
     });
 };
 
--- V1/src/services/Section.js
+++ V1/src/services/Section.js
@@ -1,28 +1,16 @@
-const Section = require('../model/Section');
+const BaseService = require('./BaseService');
+const BaseModel = require('../model/Section');
 
-const list = (where) => {
-  //console.log(where);
-  return Section.find(where || {}).populate({
-    path: 'user_id',
-    select: 'full_name email',
-  });
-};
+class Section extends BaseService {
+  constructor() {
+    super(BaseModel);
+  }
+  list(where) {
+    return BaseModel.find(where || {}).populate({
+      path: 'user_id',
+      select: 'full_name email',
+    });
+  }
+}
 
-const insert = (data) => {
-  return new Section(data).save();
-};
-
-const modify = (data, id) => {
-  return Section.findByIdAndUpdate(id, data, { new: true });
-};
-
-const remove = (id) => {
-  return Section.findByIdAndDelete(id);
-};
-
-module.exports = {
-  insert,
-  list,
-  modify,
-  remove,
-};
+module.exports = Section;
